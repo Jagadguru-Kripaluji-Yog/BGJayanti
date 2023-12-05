178c2014f2b8d05fb0bea0338f64760a048256c9
PranayPant <ppantg1@gmail.com>
2023-12-04 19:55:51 -0600

    updates

--- src/components/scroll-wrapper/index.tsx
+++ src/components/scroll-wrapper/index.tsx
@@ -10,7 +10,7 @@ export function ScrollWrapper({ scrollTo }: ScrollWrapperProps) {
     <div className="absolute w-full h-full flex z-10 top-0 left-0">
       <a
         href={scrollTo}
-        className="hover:cursor-pointer absolute bottom-0 md:ml-[45%] ml-[50%] mb-10 md:text-[100px] text-[30px] text-white animate-bounce"
+        className="hover:cursor-pointer hover:text-white absolute bottom-0 md:ml-[45%] ml-[50%] mb-10 md:text-[100px] text-[30px] text-white animate-bounce"
       >
         <FaChevronDown />
       </a>
